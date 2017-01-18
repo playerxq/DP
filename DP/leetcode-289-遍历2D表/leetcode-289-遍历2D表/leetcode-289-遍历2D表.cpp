d0c1cfa0e278eae7af8a3b63885d320c9fbfef71
playerxq <1062842783@qq.com>
2017-01-18 23:19:46 +0800

    leetcode-289
    leetcode-289

---
+++ DP/leetcode-289-遍历2D表/leetcode-289-遍历2D表/leetcode-289-遍历2D表.cpp
@@ -0,0 +1,60 @@
+// leetcode-289-遍历2D表.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <cstdio>
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include<iomanip>
+#include <ctype.h>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	void gameOfLife(vector<vector<int>>& board) {
+		int d[][2] = { { 1, -1 }, { 1, 0 }, { 1, 1 }, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
+		for (int i = 0; i < board.size(); i++){
+			for (int j = 0; j < board[0].size(); j++){
+				int live = 0;
+				for (int k = 0; k < 8; k++){
+					int x = d[k][0] + i;
+					int y = d[k][1] + j;
+					if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size()) {
+						continue;
+					}
+					if (board[x][y] & 1) {
+						live++;
+					}
+				}
+				if (board[i][j] == 0) {
+					if (live == 3){
+						board[i][j] = 2; // 2 : (10)
+					}
+				}
+				// 活的
+				else {
+					if (live < 2 || live > 3){
+						board[i][j] = 1; // 1 : (01)
+					}
+					else{
+						board[i][j] = 3; // 3 : (11)   
+					}
+				}
+			}
+		}
+		for (int i = 0; i < board.size(); i++){
+			for (int j = 0; j < board[0].size(); j++){
+				board[i][j] >>= 1;
+			}
+		}
+	}
+};
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
